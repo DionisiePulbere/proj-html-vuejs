b78fda9f07179d3191b2c4fa2ae26b6b0445d5bb
DionisiePulbere <dionisiepulbere@gmail.com>
2024-04-24 18:18:13 +0200

    feat: add section gallery

--- src/components/MainGallery.vue
+++ src/components/MainGallery.vue
@@ -7,10 +7,64 @@
 </script>
 
 <template>
-    
+    <section class="gallery">
+        <div class="buttons">
+            <div class="logo mb-2">
+                <img class="p-2" src="../assets/img/svg/svg-5.svg" alt="">
+            </div>
+            <div class="cart">
+                <img class="p-2" src="../assets/img/svg/svg-7.svg" alt="">
+            </div>
+        </div>
+        <div class="btn-radious">
+                <i class="fa-solid fa-arrow-up-long"></i>
+            </div>
+        <div class="container-fluid">
+            <i class="fa-solid fa-play"></i>
+            <img src="../assets/img/h1-img-04.jpg" alt="">
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
 @use '../style/partials/variables' as *;
 
+img{
+    width: 100%;
+}
+.container-fluid{
+
+    i{
+        font-size: 50px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50% , -50%);
+        color: white;       
+    }
+}
+section{
+    position: relative;
+}
+
+.btn-radious{
+    top: 20%;
+}
+.buttons{
+    position: absolute;
+    top: -20px;
+    right: 0;
+    background-color: transparent;
+    img{
+        width: 40px;
+    }
+}
+
+.cart{
+    background-color: white;
+}
+
+.logo{
+    background-color: $brand-primary;
+}
 </style>
